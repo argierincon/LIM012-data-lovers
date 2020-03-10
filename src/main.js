72f46bcbad2e9d90fd811eb42572c2c68f067dbf
DianaMS <dmartinezs@uni.pe>
2020-03-10 18:22:58 -0500

    mostrar data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import { example } from './data.js';
-// import data from './data/atletas/atletas.js';
-// import data from './data/lol/lol.js';
+//import { example } from './data.js';
+
+
 import data from './data/pokemon/pokemon.js';
 
-console.log(example, data);
+const pokeData = document.getElementById('pokeData');
+for (let i=0; i<data.pokemon.length ; i += 1){
+    const pokeDat = `
+    <section id="pokeCard" class="pokeCard">
+        <div class="number" id="number">${data.pokemon[i].num}</div>
+        <div class="imgPoke" id="imgPoke"><img src=${data.pokemon[i].img} alt="" class="imgPkm"></div>
+        <div class="namePoke" id="namePoke">${data.pokemon[i].name.toUpperCase()}</div>
+      </section>
+    `;
+    pokeData.innerHTML += pokeDat; 
+}
+
+
+console.log( data);
