81875c31e90a3123a44826af6e6690175f3c4c6f
DianaMS <dmartinezs@uni.pe>
2020-03-11 12:43:55 -0500

    forEach

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-//import { example } from './data.js';
-
-
 import data from './data/pokemon/pokemon.js';
 
 const pokeData = document.getElementById('pokeData');
-for (let i=0; i<data.pokemon.length ; i += 1){
-    const pokeDat = `
-    <section id="pokeCard" class="pokeCard">
-        <div class="number" id="number">${data.pokemon[i].num}</div>
-        <div class="imgPoke" id="imgPoke"><img src=${data.pokemon[i].img} alt="" class="imgPkm"></div>
-        <div class="namePoke" id="namePoke">${data.pokemon[i].name.toUpperCase()}</div>
-      </section>
-    `;
-    pokeData.innerHTML += pokeDat; 
-}
 
+let showData = '';
+data.pokemon.forEach((elem) => {
+  const pokeDat = `
+  <section id="pokeCard" class="pokeCard">
+  <div class="number" id="number">${elem.num}</div>
+  <div class="imgPoke" id="imgPoke"><img src=${elem.img} alt="" class="imgPkm"></div>
+  <div class="namePoke" id="namePoke">${elem.name.toUpperCase()}</div>
+  </section>
+  `;
+  showData += pokeDat;
+});
 
-console.log( data);
+pokeData.innerHTML = showData;
+
+console.log(data);
