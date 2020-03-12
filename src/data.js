bf2152dbf050807ddebc99f8ab054629b9f47866
DianaMS <dmartinezs@uni.pe>
2020-03-12 12:08:03 -0500

    creando funcion orden A-Z

--- src/data.js
+++ src/data.js
@@ -1,5 +1,26 @@
 // estas funciones son de ejemplo
 
-export const example = () => 'example';
+export const dataCards = (dataPokemon) => {
+  let showData = '';
+  dataPokemon.forEach((element) => {
+    const pokeDat = `
+        <section id="pokeCard" class="pokeCard">
+        <div class="number" id="number">${element.num}</div>
+        <div class="imgPoke" id="imgPoke"><img src=${element.img} alt="" class="imgPkm"></div>
+        <div class="namePoke" id="namePoke">${element.name.toUpperCase()}</div>
+        </section>
+   `;
+    showData += pokeDat;
+  });
+  return showData;
+};
 
-export const anotherExample = () => 'OMG';
+export const orderData = (data) => {
+  const order = data.sort((a, b) => {
+    if (a.name > b.name) {
+      return 1;
+    }
+    return -1;
+  });
+  return order;
+};
