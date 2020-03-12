1e59b1d14315d4f491f7494fe6907168f0001e2c
Diana Martinez Sifuentes <60928490+DianaMS@users.noreply.github.com>
2020-03-12 09:12:42 -0500

    showData (#4)
    * probando
    
    * mostrar data
    
    * forEach
    
    * pruebita2
    
    * probando2
    
    Co-authored-by: Argie Rincon <59261032+argierdfj@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -3,17 +3,15 @@ import data from './data/pokemon/pokemon.js';
 const pokeData = document.getElementById('pokeData');
 
 let showData = '';
-data.pokemon.forEach((elem) => {
+data.pokemon.forEach((element) => {
   const pokeDat = `
   <section id="pokeCard" class="pokeCard">
-  <div class="number" id="number">${elem.num}</div>
-  <div class="imgPoke" id="imgPoke"><img src=${elem.img} alt="" class="imgPkm"></div>
-  <div class="namePoke" id="namePoke">${elem.name.toUpperCase()}</div>
+  <div class="number" id="number">${element.num}</div>
+  <div class="imgPoke" id="imgPoke"><img src=${element.img} alt="" class="imgPkm"></div>
+  <div class="namePoke" id="namePoke">${element.name.toUpperCase()}</div>
   </section>
   `;
   showData += pokeDat;
 });
 
 pokeData.innerHTML = showData;
-
-console.log(data);
