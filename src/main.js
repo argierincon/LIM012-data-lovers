6a8eab7be08b1f30572c0e7273ee7cf59309fc8d
DianaMS <dmartinezs@uni.pe>
2020-03-12 12:09:25 -0500

    importar funciones

--- src/main.js
+++ src/main.js
@@ -1,18 +1,13 @@
 import data from './data/pokemon/pokemon.js';
+import { dataCards, orderData } from './data.js';
+
 
 const pokeData = document.getElementById('pokeData');
-
-let showData = '';
-data.pokemon.forEach((element) => {
-  const pokeDat = `
-  <section id="pokeCard" class="pokeCard">
-  <div class="number" id="number">${element.num}</div>
-  <div class="imgPoke" id="imgPoke"><img src=${element.img} alt="" class="imgPkm"></div>
-  <div class="namePoke" id="namePoke">${element.name.toUpperCase()}</div>
-  </section>
-  `;
-  showData += pokeDat;
-});
-
-pokeData.innerHTML = showData;
+pokeData.innerHTML = dataCards(data.pokemon);
 // console.log(data);
+
+const pokeData1 = document.getElementById('pokeData1');
+pokeData1.innerHTML = dataCards(orderData(data.pokemon));
+
+const pokeData2 = document.getElementById('pokeData2');
+pokeData2.innerHTML = dataCards(orderData(data.pokemon).reverse());
