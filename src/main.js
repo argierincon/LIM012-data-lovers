a336166a5a5f066bf3a551b9f849058c3d5a5571
Diana Martinez Sifuentes <60928490+DianaMS@users.noreply.github.com>
2020-03-13 09:31:02 -0500

    cambios recibidos (#6)
    * probando
    
    * mostrar data
    
    * forEach
    
    * pruebita2
    
    * probando2
    
    * showData2
    
    * cambio tamaño fuente
    
    * cambios creacion de seccion
    
    * creando funcion orden A-Z
    
    * se agrego dos section
    
    * importar funciones
    
    * funciones mostrar, orderAZ, orderZA, orderMaxCP(ascen,descen)
    
    * funciones mostrar, ordenar A-Z/Z-A, orden maxCP
    
    Co-authored-by: Argie Rincon <59261032+argierdfj@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import data from './data/pokemon/pokemon.js';
-import { dataCards, orderData } from './data.js';
-
+import {
+  dataCards,
+  orderData,
+  dataMaxCP,
+  orderMaxCP,
+} from './data.js';
 
 const pokeData = document.getElementById('pokeData');
 pokeData.innerHTML = dataCards(data.pokemon);
@@ -10,3 +14,9 @@ pokeData1.innerHTML = dataCards(orderData(data.pokemon));
 
 const pokeData2 = document.getElementById('pokeData2');
 pokeData2.innerHTML = dataCards(orderData(data.pokemon).reverse());
+
+const pokeData3 = document.getElementById('pokeData3');
+pokeData3.innerHTML = dataMaxCP(orderMaxCP(data.pokemon));
+
+const pokeData4 = document.getElementById('pokeData4');
+pokeData4.innerHTML = dataMaxCP(orderMaxCP(data.pokemon).reverse());
