4057bf2b2fb0bca24cdeb575627636e4324ba2e9
argierdfj <argierincon@gmail.com>
2020-03-14 16:50:08 -0500

    tarjeta giratoria

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import data from './data/pokemon/pokemon.js';
 import {
   orderData,
   orderMaxCP,
-// dataCards,
+  dataCards,
 // dataMaxCP,
 } from './data.js';
 
@@ -15,26 +15,29 @@ const pokeData = document.getElementById('pokeData');
 
 // diana: agregue la parte delantera y trasera de la tarjeta, es más eficiente y vi que era
 //   más practico para darle el efecto de giro, lo vi en un video x
-const dataCards = (dataPokemon) => {
-  let showData = '';
-  dataPokemon.forEach((element) => {
-    const pokeDat = `
-            <section id="pokeCard" class="pokeCard">
-            <div class="number" id="number">${element.num}</div>
-            <div class="imgPoke" id="imgPoke"><img src=${element.img} alt="" class="imgPkm"></div>
-            <div class="namePoke" id="namePoke">${element.name.toUpperCase()}</div>
-            </section>
+// const dataCards = (dataPokemon) => {
+//   let showData = '';
+//   dataPokemon.forEach((element) => {
+//     const pokeDat = `
+//             <section id="pokeCard" class="pokeCard">
+//             <div class="number" id="number">${element.num}</div>
+//             <div class="imgPoke" id="imgPoke"><img src=${element.img}
+//                  alt="" class="imgPkm"></div>
+//             <div class="namePoke" id="namePoke">${element.name.toUpperCase()}</div>
+//             </section>
 
-            <section id="pokeCard2" class="pokeCard">
-            <div class="number" id="number">${element.num}</div>
-            <div class="maxCP" id="maxCP"><span class="cp">Max-CP: ${element.stats['max-cp']}</span></div>
-            </section>
-       `;
-    showData += pokeDat;
-  });
-  pokeData.innerHTML = showData;
-};
-dataCards(data.pokemon);
+//             <section id="pokeCard2" class="pokeCard">
+//             <div class="number" id="number">${element.num}</div>
+//             <div class="maxCP" id="maxCP">
+//                <span class="cp">Max-CP: ${element.stats['max-cp']}</span></div>
+//             </section>
+//        `;
+//     showData += pokeDat;
+//   });
+//
+// };
+const showData = dataCards(data.pokemon);
+pokeData.innerHTML = showData;
 
 const orderAlfabetic = document.querySelector('#order');
 orderAlfabetic.addEventListener('change', () => {
