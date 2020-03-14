5822b59deab16811d8f4f23bed55b0dd8cd566f0
argierdfj <argierincon@gmail.com>
2020-03-14 16:31:03 -0500

    cambios temporales para el merge

--- src/data.js
+++ src/data.js
@@ -2,13 +2,31 @@
 
 export const dataCards = (dataPokemon) => {
   let showData = '';
-  dataPokemon.forEach((element) => {
+  dataPokemon.forEach((element, idx) => {
+    if (idx > 5) return;
     const pokeDat = `
+  <section class="data">
+    <section class="card">
+      <section class="side front">
         <section id="pokeCard" class="pokeCard">
-        <div class="number" id="number">${element.num}</div>
-        <div class="imgPoke" id="imgPoke"><img src=${element.img} alt="" class="imgPkm"></div>
-        <div class="namePoke" id="namePoke">${element.name.toUpperCase()}</div>
+          <div class="number" id="number">${element.num}</div>
+          <div>
+            <div class="imgPoke" id="imgPoke"><img src=${element.img} alt="" class="imgPkm"></div>
+            <div class="namePoke" id="namePoke">${element.name.toUpperCase()}</div>
+          </div>
         </section>
+      </section>
+
+      <section class="side back">
+        <section id="pokeCard2" class="pokeCard">
+          <div class="number" id="number">${element.num}</div>
+          <div>
+            <div class="maxCP" id="maxCP"><span class="cp">Max-CP: ${element.stats['max-cp']}</span></div>
+          </div>
+        </section>
+      </section>
+    </section>
+  </section>     
    `;
     showData += pokeDat;
   });
@@ -25,19 +43,35 @@ export const orderData = (data) => {
   return order;
 };
 
-export const dataMaxCP = (dataPokemon) => {
-  let showData = '';
-  dataPokemon.forEach((element) => {
-    const pokeDat = `
-        <section id="pokeCard2" class="pokeCard">
-        <div class="number" id="number">${element.num}</div>
-        <div class="maxCP" id="maxCP"><span class="cp">Max-CP: ${element.stats['max-cp']}</span></div>
-        </section>
-   `;
-    showData += pokeDat;
-  });
-  return showData;
-};
+// export const dataMaxCP = (dataPokemon) => {
+//   let showData = '';
+//   dataPokemon.forEach((element) => {
+//     const pokeDat = `
+//     <section id="pokeData" class="data contain">
+//     <section class="card">
+//     <section class="side front">
+//       <section id="pokeCard" class="pokeCard">
+//         <div class="number" id="number">${element.num}</div>
+//         <div class="imgPoke" id="imgPoke"><img src=${element.img} alt="" class="imgPkm"></div>
+//         <div class="namePoke" id="namePoke">${element.name.toUpperCase()}</div>
+//       </section>
+//     </section>
+
+//     <section class="side back">
+//       <section id="pokeCard2" class="pokeCard">
+//         <div class="number" id="number">${element.num}</div>
+//         <div class="maxCP" id="maxCP"><span class="cp">
+//            Max-CP: ${element.stats['max-cp']}
+//          </span></div>
+//       </section>
+//     </section>
+//   </section>
+// </section>
+//    `;
+//     showData += pokeDat;
+//   });
+//   return showData;
+// };
 
 export const orderMaxCP = (data) => {
   const order = data.sort((a, b) => a.stats['max-cp'] - b.stats['max-cp']);
--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
 import data from './data/pokemon/pokemon.js';
 import {
   dataCards,
-  orderData,
-  dataMaxCP,
-  orderMaxCP,
+  // orderData,
+  // dataMaxCP,
+  // orderMaxCP,
 } from './data.js';
 
 const pokeData = document.getElementById('pokeData');
 pokeData.innerHTML = dataCards(data.pokemon);
 
-const pokeData1 = document.getElementById('pokeData1');
-pokeData1.innerHTML = dataCards(orderData(data.pokemon));
+// const pokeData1 = document.getElementById('pokeData1');
+// pokeData1.innerHTML = dataCards(orderData(data.pokemon));
 
-const pokeData2 = document.getElementById('pokeData2');
-pokeData2.innerHTML = dataCards(orderData(data.pokemon).reverse());
+// const pokeData2 = document.getElementById('pokeData2');
+// pokeData2.innerHTML = dataCards(orderData(data.pokemon).reverse());
 
-const pokeData3 = document.getElementById('pokeData3');
-pokeData3.innerHTML = dataMaxCP(orderMaxCP(data.pokemon));
+// const pokeData3 = document.getElementById('pokeData3');
+// pokeData3.innerHTML = dataMaxCP(orderMaxCP(data.pokemon));
 
-const pokeData4 = document.getElementById('pokeData4');
-pokeData4.innerHTML = dataMaxCP(orderMaxCP(data.pokemon).reverse());
+// const pokeData4 = document.getElementById('pokeData4');
+// pokeData4.innerHTML = dataMaxCP(orderMaxCP(data.pokemon).reverse());
