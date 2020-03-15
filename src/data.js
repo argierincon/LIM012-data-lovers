b127c4b20a5c8674b3388e365c6362816ed98003
argierdfj <argierincon@gmail.com>
2020-03-15 16:30:52 -0500

    mostrando data del pokemon.js

--- src/data.js
+++ src/data.js
@@ -3,3 +3,33 @@
 export const example = () => 'example';
 
 export const anotherExample = () => 'OMG';
+
+
+export const showCards = (arrPkm) => {
+  let cards = '';
+  arrPkm.forEach((element) => {
+    const templateCard = `<section class="container">
+      <section class="card">
+        <div class="side front">
+          <div class="pokeNumber">${element.num}</div>
+          <div class="pokeImgName">
+            <img src="${element.img}" alt="Pokémon Image" class="pokeImg">
+            <p class="pokeName">${element.name}</p>
+          </div>
+        </div>
+        <div class="side back">
+          <div class="pokeNumber">${element.num}</div>
+          <div class="pokeInfo">
+            <p><span>Tipo: </span>${element.type[0]}</p>
+            <p><span>Región: </span>${element.generation.name}</p>
+            <p><span>Max-CP: </span>${element.stats['max-cp']}</p>
+            <p><span>Huída: </span>${(element.encounter['base-flee-rate'] * 100).toFixed(2)}%</p>
+            <p><span>Aparición: </span>${(element['spawn-chance'] * 100).toFixed(2)}%</p>
+          </div>
+        </div>
+      </section>
+    </section>`;
+    cards += templateCard;
+  });
+  return cards;
+};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import { example } from './data.js';
+import { showCards } from './data.js';
 // import data from './data/atletas/atletas.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 
-console.log(example, data);
+console.log(data.pokemon[0]);
+
+const dataContainer = document.getElementById('dataContainer');
+dataContainer.innerHTML = showCards(data.pokemon);
