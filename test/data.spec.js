53dc4f7090c8cf4460c6e21a5b341e2724f6f43a
argierdfj <argierincon@gmail.com>
2020-03-19 12:23:09 -0500

    tests de las funciones huida y aparicion

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,8 +1,11 @@
+import data from '../src/data/pokemon/pokemon.js';
 import {
   orderData,
   orderMaxCP,
   orderFilterType,
   orderFilterRegion,
+  filterFleeRate,
+  filterSpawn,
 } from '../src/data.js';
 
 const firstTestExpect = [
@@ -181,3 +184,52 @@ describe('orderFilterRegion', () => {
 
   it('Debe retornar [] si no encentra una región', () => {
     expect(orderFilterRegion(firstTestExpect, 'name', 'johto')).toEqual([]);
+  });
+});
+
+// TEST DE LA FUNCIÓN POR % HUÍDA
+describe('filterFleeRate', () => {
+  it('debería ser una función', () => {
+    expect(typeof filterFleeRate).toBe('function');
+  });
+
+  it('debería retornar "raikou" para "lowRate"', () => {
+    expect(filterFleeRate(data.pokemon, 'lowRate')[0].name).toBe('raikou');
+  });
+
+  it('debería retornar "hoppip" para "mediumRate"', () => {
+    expect(filterFleeRate(data.pokemon, 'mediumRate')[0].name).toBe('hoppip');
+  });
+
+  it('debería retornar "abra" para "highRate"', () => {
+    expect(filterFleeRate(data.pokemon, 'highRate')[0].name).toBe('abra');
+  });
+
+  it('debería retornar "pichu" para "no en captura"', () => {
+    expect(filterFleeRate(data.pokemon, 'no en captura')[0].name).toBe('pichu');
+  });
+});
+
+
+// TEST DE LA FUNCIÓN POR % HUÍDA
+describe('filterSpawn', () => {
+  it('debería ser una función', () => {
+    expect(typeof filterSpawn).toBe('function');
+  });
+
+  it('debería retornar "chikorita" para "nula"', () => {
+    expect(filterSpawn(data.pokemon, 'nula')[0].name).toBe('chikorita');
+  });
+
+  it('debería retornar "dragonite" para "lowSpawn"', () => {
+    expect(filterSpawn(data.pokemon, 'lowSpawn')[0].name).toBe('dragonite');
+  });
+
+  it('debería retornar "psyduck" para "mediumSpawn"', () => {
+    expect(filterSpawn(data.pokemon, 'mediumSpawn')[0].name).toBe('psyduck');
+  });
+
+  it('debería retornar "zubat" para "highSpawn"', () => {
+    expect(filterSpawn(data.pokemon, 'highSpawn')[0].name).toBe('zubat');
+  });
+});
